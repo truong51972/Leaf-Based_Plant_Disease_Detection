21fe0b790b2a7618ba930c88185c0294f1323c80
truong51972 <tranquoctruong20@gmail.com>
2024-05-14 17:29:09 +0700

    remove ddns file, add inside request_api.py. Refactor request_api.py and add "predict" api

--- packages/request_api.py
+++ packages/request_api.py
@@ -1,34 +1,138 @@
 import requests
-from packages import get_ip_from_ddns
-from packages.encode_deconde import encrypt_password
-    
+import dns.resolver
+
+"""
+    Contain all request functions for Website.
+"""
+
 my_ddns = 'truong51972.ddns.net'
 port = 8000
 
 base_url = "http://{}:{}"
 
-def check_login(item: dict):
-    ip = get_ip_from_ddns.get(my_ddns)
-    url = base_url.format(ip, port) + '/check-login'
-    print(url)
+def __ddns_to_ip(domain: str) -> str:
+    answers = dns.resolver.resolve(domain, 'A')
+    return next(iter(answers)).address
 
-    response = requests.post(url, json= item)
+def __request(api_name: str, json: dict):
+    try:
+        ip = __ddns_to_ip(my_ddns)
+        url = base_url.format(ip, port) + api_name
+        print("Sending request to: '{url}'!")
+        response = requests.post(url, json= json)
+    except:
+        response = {
+            'message' : 'Server not Found!',
+            'code': '404'
+        }
+    return response
+
+def check_login(item: dict):
+    """
+    Make a request to database server to check user login.
+
+    Args:
+        item: dict[str, str]
+
+    Returns:
+        response = {
+            'message' : 'message!',
+            'code': 'error code!'
+        }
+        
+    Example:
+
+    >>> item = {
+            'user_name' : 'user name',
+            'password' : 'password'
+        }
+    >>> check_login(item = item)
+    {'message' : '...','code': 'XXX'}
+    """
+    api_name = '/check-login'
+    
+    response = __request(api_name, item)
     return response
 
 def create_new_user(item: dict):
-    ip = get_ip_from_ddns.get(my_ddns)
-    url = base_url.format(ip, port) + '/create-new-user'
-    print(url)
+    """
+    Make a request to database server to create new user.
+
+    Args:
+        item: data to request.
+
+    Returns:
+        response = {
+            'message' : 'message!',
+            'code': 'error code!'
+        }
+        
+    Example:
+
+    >>> item = {
+            'user_name' : 'user name',
+            'password' : 'password'
+        }
+    >>> create_new_user(item = item)
+    {'message' : '...','code': 'XXX'}
+    """
+
+    api_name = '//create-new-user'
     
-    response = requests.post(url, json= item)
+    response = __request(api_name, item)
+    return response
+
+def predict(item: dict):
+    """
+    Make a request to database server to create new user.
+
+    Args:
+        item: data to request.
+
+    Returns:
+        response = {
+            'message' : 'message!',
+            'code': 'error code!',
+            'result': {
+                'class_name' : 'class name',
+                'description' : 'description',
+                'solution' : 'solution'
+            }
+        }
+        
+    Example:
+
+    >>> item = {
+            'user_info': {
+                'info': {
+                    'user_name' : 'user name',
+                    'password' : 'password'
+                },
+            'image_info' : {
+                'image' : 'decoded image',
+                'date' : 'DD-MM-YYYY'
+            }
+        }
+    >>> predict(item = item)
+    {
+        'message' : 'message!',
+        'code': 'error code!',
+        'result': {
+            'class_name' : 'class name',
+            'description' : 'description',
+            'solution' : 'solution'
+        }
+    }
+    """
+    
+    api_name = '/predict'
+    
+    response = __request(api_name, item)
     return response
 
 if __name__ == '__main__':
-    encrypted_password = encrypt_password('chao em').decode()
     item = {
-        'user_name' : 'admin',
-        'password': encrypted_password
+        'user_name' : 'user name',
+        'password' : 'password'
     }
-
-    response = check_login(item)
-    print(response.json())
+    print(predict(item))
