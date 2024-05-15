e485b385006be70f89888d036422034e8d5e1309
NguyenThien <ngonguyenthien25082003@gmail.com>
2024-05-15 18:57:34 +0700

    test

--- web/app.py
+++ web/app.py
@@ -1,5 +1,21 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ExifTags
+
+def correct_orientation(image):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = image._getexif()
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        pass
+    return image
 
 def app():
     st.title("Ảnh Đầu Vào và Đầu Ra")
@@ -7,4 +23,9 @@ def app():
 
     if uploaded_image is not None:
         image = Image.open(uploaded_image)
-        st.image(image, use_column_width=True)
+        # Correct orientation based on EXIF data
+        corrected_image = correct_orientation(image)
+        st.image(corrected_image, use_column_width=True)
+
+if __name__ == "__main__":
+    app()
