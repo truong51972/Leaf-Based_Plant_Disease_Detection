740f368db8e5cb3e87449aefcc0842927b4ef776
NguyenThien <ngonguyenthien25082003@gmail.com>
2024-05-20 09:31:25 +0700

    update4: The born of history2.2_fix datetime2

--- web/history.py
+++ web/history.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from packages.request_api import get_history
 import pandas as pd
-import pytz
 
 def __url_gen(x):
     return 'data:image/jpeg;base64,' + x
 
-def utc(dt):
-    local_tz = pytz.timezone('Asia/Ho_Chi_Minh')
-    dt_local = local_tz.localize(dt)
-    return dt_local.astimezone(pytz.utc).astimezone(local_tz)
+def time_zone():
+    server_timezone = pytz.timezone('UTC')
+    server_time = datetime.utcnow()
 
 def display_history():
     item = {
@@ -17,15 +15,10 @@ def display_history():
             'password': st.session_state.get('encrypted_password')
         }
     
-    if st.button("Xem thông tin."):
+    if st.button("Xem thông tin"):
         response =  get_history(item=item).json()
         
         df_history = pd.DataFrame(response['history'])
-
-        if 'Ngày chụp' in df_history.columns:
-            df_history['Ngày chụp'] = pd.to_datetime(df_history['Ngày chụp'])
-            df_history['Ngày chụp'] = df_history['Ngày chụp'].apply(lambda x: utc(x))
-        
 
         df_history['Ảnh gốc'] = df_history['Ảnh gốc'].apply(__url_gen)
         df_history['Ảnh phân tích'] = df_history['Ảnh phân tích'].apply(__url_gen)
@@ -33,7 +26,6 @@ def display_history():
         st.data_editor(
             df_history,
             column_config={
-                'Ngày chụp': st.column_config.DatetimeColumn(format='YYYY-MM-DD HH:mm:ss', timezone='Asia/Ho_Chi_Minh'),
                 'Ảnh gốc': st.column_config.ImageColumn(width='small'),
                 'Ảnh phân tích':st.column_config.ImageColumn(width='small')
             },
