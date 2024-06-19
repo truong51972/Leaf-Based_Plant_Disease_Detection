0229df205fb1406cf3c6494cfe02f2e13cd9b627
truong51972 <tranquoctruong20@gmail.com>
2024-06-19 08:47:35 +0700

    commit to update model from truong-img-classifier

---
+++ convinience_scripts/update_model.bat
@@ -0,0 +1,6 @@
+git add .
+git commit -m 'commit to update model from truong-img-classifier'
+git switch truong-img-classifier
+git pull
+git switch main
+git checkout truong-img-classifier -- model
--- update_database.bat
+++
@@ -1,6 +0,0 @@
-git add .
-git commit -m 'commit to update database'
-git switch hieu
-git pull
-git switch main
-git checkout hieu -- database
