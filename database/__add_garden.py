c039a2b93264afcd35fa55631886705d31c3e374
truong51972 <tranquoctruong20@gmail.com>
2024-06-24 15:20:32 +0700

    update database successfully!

---
+++ database/__add_garden.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def add_garden_to_db(managerName:str,
+                     gardenName:str, 
+                     lineID:list[int],
+                     plantName:str, 
+                     con):
+    
+    cur = con.cursor()
+    cur.execute(f'''
+        SELECT userID 
+        FROM USER
+        WHERE userName = '{managerName}'
+        ''')
+    
+    managerID = cur.fetchall()[0][0]
+
+    print(managerID)
+
+    cur.execute(f'''
+                INSERT INTO GARDEN (gardenName, managerID, plantName)
+                VALUES ('{gardenName}', {managerID}, '{plantName}')
+                ''')
+    
+    con.commit()
+
+    print('insert garden')
+
+    cur.execute(f'''
+        SELECT last_insert_rowid()
+        ''')
+    
+    gardenID = cur.fetchall()[0][0]
+
+    for line in lineID:
+        cur.execute(f'''
+                    INSERT INTO LOCATION (gardenID, lineID)
+                    VALUES ({gardenID}, {line})
+                    ''')
+        
+
+
+        con.commit()
+
+    print('complete')
--- database/__load_employee_pic_count.py
+++ database/__load_employee_pic_count.py
@@ -10,30 +10,31 @@ def load_employee_pic_count(managerName:str,
         WHERE userName = '{managerName}'
         ''')
     
-    userID = cur.fetchall()[0][0]
+    managerID = cur.fetchall()[0][0]
 
     cur.execute(f'''
-                SELECT userName, gardenNum, lineNum, count(*) as pic_count FROM
-                (SELECT USER.userName as userName, LOCATION.gardenNum as gardenNum, LOCATION.lineNum as lineNum
+                SELECT userName, gardenName, lineNum, count(*) as pic_count FROM
+                (SELECT USER.userName as userName, GARDEN.gardenName as gardenName, LOCATION.lineNum as lineNum
                 FROM USER
                 RIGHT JOIN USER_LOCATION ON USER.userID = USER_LOCATION.userID
                 JOIN USER_PIC ON USER_PIC.userID = USER.userID
                 JOIN PIC ON PIC.picID = USER_PIC.picID
                 JOIN LOCATION on PIC.locationID = LOCATION.locationID
+                JOIN GARDEN on LOCATION.gardenNum = GARDEN.gardenID
                 WHERE USER.userID in (SELECT e.userID AS employeeID
                                     FROM USER e
                                     INNER JOIN USER m ON e.managerID = m.userID
-                                    WHERE m.userID = {userID})
+                                    WHERE m.userID = {managerID})
                 GROUP BY USER.userID, USER_LOCATION.userID, USER_PIC.userID, PIC.picID)
-                GROUP BY userName, gardenNum, lineNum
+                GROUP BY userName, gardenName, lineNum
                 ''')
     
     data = cur.fetchall()
 
     employeeName = list(map(lambda x: x[0], data))
-    gardenNum = list(map(lambda x: x[1], data))
+    gardenName = list(map(lambda x: x[1], data))
     lineNum = list(map(lambda x: x[2], data))
     picCount = list(map(lambda x: x[3], data))
 
-    return (employeeName, gardenNum, lineNum, picCount)
+    return (employeeName, gardenName, lineNum, picCount)
     
