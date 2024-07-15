b3f37d715d1152055b2d276c2a18183a279f2d3e
truong51972 <tranquoctruong20@gmail.com>
2024-07-15 11:03:40 +0700

    update multi cnn model

--- model/disease_detection.py
+++ model/disease_detection.py
@@ -14,28 +14,52 @@ from model.grad_cam import Grad_cam
 import asyncio
 
 class AI_model:
-    def __init__(self, path_to_model: str):
+    def __init__(self, paths_to_model: dict):
+        """
+        Args:
+            paths_to_model : dict = {'model_name', 'path/to/model'}
+        """
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print(f"Device: {self.device}")
         self.sam_model = Sam_model(device= self.device)
+
+        self.cnn_models = {}
+        self.grad_cam_models = {}
         
-        self.cnn_model = Cnn_model(path_to_model=path_to_model, device= self.device)
-
-        model = self.cnn_model.get_model()
-        self.grad_cam = Grad_cam(model=model, model_name= self.cnn_model.model_name)
-
+        for model_name, path_to_model in paths_to_model.items():
+            try:
+                cnn_model = Cnn_model(path_to_model=path_to_model, device= self.device)
+                
+                self.cnn_models[model_name] = cnn_model
+                self.grad_cam_models[model_name] = Grad_cam(model=cnn_model.get_model(), model_name= cnn_model.model_name)
+            except FileNotFoundError:
+                print(f'[ERROR] Model not found in {path_to_model}')
         try:
             self.best_threshold_df = pd.read_excel(path_to_model + '/best_threshold.xlsx', index_col=0)
         except:
             self.best_threshold_df = None
             
-    def _predict(self, img: Image):
+    def class_name_to_idx(self, class_name: str, model_name: str | None = None, verbose: bool = False):
+        if model_name is not None:
+            print(f"[WARNING] '{model_name}' is not defined!")
+        cnn_model = self.cnn_models.get(model_name, list(self.cnn_models.values())[0])
+        return cnn_model.class_name_to_idx[class_name]
+        
+    def _predict(self, img: Image, model_name: str | None = None, verbose: bool = False):
         pointed_img = self.sam_model.plot_points(img)
         removed_bg_img = self.sam_model.remove_bg(img)
 
-        predict_logit, predicted_class, score = self.cnn_model._predict(removed_bg_img)
+        if verbose and (model_name not in self.cnn_models.keys()):
+            if model_name is not None:
+                print(f"[WARNING] '{model_name}' is not defined!")
+            print(f"[WARNING] Using '{list(self.cnn_models.keys())[0]}' model!")
+            
+        cnn_model = self.cnn_models.get(model_name, list(self.cnn_models.values())[0])
+        grad_cam = self.grad_cam_models.get(model_name, list(self.grad_cam_models.values())[0])
+            
+        predict_logit, predicted_class, score = cnn_model._predict(removed_bg_img)
 
-        grayscale_cam, visualization = self.grad_cam.visualize(removed_bg_img, predict_logit, threshold= 0.5)
+        grayscale_cam, visualization = grad_cam.visualize(removed_bg_img, predict_logit, threshold= 0.5)
         
         results = {
             "image" : img,
@@ -48,11 +72,11 @@ class AI_model:
         }
         return results
         
-    async def predict(self, img: Image):
+    async def predict(self, img: Image, model_name: str | None = None, verbose: bool = False):
         """
         Args:
             img: PIL.Image()
-     
+            model_name : str
         Returns:
             dict: {
                 "image" : PIL.Image(),
@@ -65,12 +89,12 @@ class AI_model:
                 "threshold" : float,                
             }
         """
-        # img = img.resize((224, 224))
+        results = self._predict(img, model_name, verbose)
         
-        results = self._predict(img)
-        
-        if self.best_threshold_df is not None:
-            class_idx = self.cnn_model.class_name_to_idx[results["class_name"]]
+        if self.best_threshold_df is not None:           
+            cnn_model = self.cnn_models.get(model_name, list(self.cnn_models.values())[0])
+                
+            class_idx = cnn_model.class_name_to_idx[results["class_name"]]
             results['threshold'] = self.best_threshold_df.loc['threshold', class_idx]
         
         return results
